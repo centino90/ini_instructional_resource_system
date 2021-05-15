c2ccc2752af35611ab323894a5a535b46b8c61f9
Honeybee-11 <77036793+Honeybee-11@users.noreply.github.com>
2021-05-15 16:29:33 +0800

    Tasknew.blade

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InstructionLogController;
 use App\Http\Controllers\SavedMaterialController;
+use App\Http\Controllers\SyllabusController;
+use App\Http\Controllers\ExamsController;
+use App\Http\Controllers\ActivitiesController;
+use App\Http\Controllers\QuizzesController;
+use App\Http\Controllers\AssignmentsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,6 +31,11 @@ Route::get('/dashboard', function () {
     return view('pages.dashboard')->with("currentPage", "Dashboard");
 })->middleware(['auth'])->name('dashboard');
 
-/* STARTING POINT */
+/* Rocky Calo */
 Route::resource('instructionlog', InstructionLogController::class);
 Route::resource('SavedMaterial', SavedMaterialController::class);
+Route::resource('Syllabus', SyllabusController::class);
+Route::resource('Exams', ExamsController::class);
+Route::resource('Activities', ActivitiesController::class);
+Route::resource('Quizzes', QuizzesController::class);
+Route::resource('Assignments', AssignmentsController::class);
